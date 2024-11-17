432a04f938ec4f5493451ccd1a055507f91404a0
Karan Singh <132837775+CARDIN00@users.noreply.github.com>
2024-11-17 12:08:29 +0530

    Add files via upload

---
+++ erc721-1.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+//ERC721 token contract
+contract BasicERC721{
+  //name and symbol
+  string public tokenName ;
+  string public tokenSymbol;
+}
+
+
